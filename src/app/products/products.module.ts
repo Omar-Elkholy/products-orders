a3e09eeba15a139b573da9ac0728934825aa827d
Omar Elkholy <omaralkholy2021@gmail.com>
2022-11-12 08:57:14 +0200

    add required components, services

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { HomeComponent } from './home/home.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    OrdersComponent,
+    OrderDetailsComponent
+  ],
   imports: [
     CommonModule,
     ProductsRoutingModule
---
+++ src/app/shared/services/core.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+/****
+ * 
+ * THIS SERVICE HAS BEEN CREATED TO HANDLE ALL THE METHODS BEING DONE ON (PRDOUCTS, ORDERS)
+ * 
+*/
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoreService {
+
+  constructor() { }
+}
