12dfdad7f7622462062fc6d44fd3b501d004a802
Omar Elkholy <omaralkholy2021@gmail.com>
2022-11-12 15:31:40 +0200

    add orders screen

--- src/app/products/orders/orders.component.ts
+++ src/app/products/orders/orders.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { CoreService } from 'src/app/shared/services/core.service';
+import { Order } from 'src/app/shared/models/order.model';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.scss']
 })
-export class OrdersComponent implements OnInit {
+export class OrdersComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  displayedColumns = ['id', 'date', 'customer', 'payment', 'details'];
+  dataSource = new MatTableDataSource<Order>();
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private coreService: CoreService
+  ) { }
 
   ngOnInit(): void {
+    this.getOrders();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  getOrders() {
+    this.coreService.getJSON<Order[]>('/assets/data/orders.json')
+      .subscribe(res => {
+        this.handleDateFormat(res);
+        this.dataSource.data = res;
+      })
+  }
+
+  // THIS FUNCTION IS USED TO HANDLE THE INVALID DATES FORMATS.
+  handleDateFormat(res: Order[]) {
+    res?.forEach(item => {
+      const date = new Date(item.OrderDate).toString();
+      item.OrderDate = date !== 'Invalid Date' ? date : '';
+    })
   }
 
 }
